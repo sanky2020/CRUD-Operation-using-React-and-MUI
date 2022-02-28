c03ba8bd64ed5ee284c9576068456fcd7ed30208
sanky2020 <sankyk2020@gmail.com>
2022-02-28 18:33:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UsersTable from './components/UsersTable'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CRUD Operation</h1>  
+      <UsersTable /> 
+    </div>
+  );
+}
+
+export default App;
