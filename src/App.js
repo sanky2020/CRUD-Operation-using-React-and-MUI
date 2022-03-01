c4727af5df4e69f2409924e0d6650e54dd019033
sanky2020 <sankyk2020@gmail.com>
2022-03-01 18:07:52 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+// import AddUser from './components/AddUser';
 import UsersTable from './components/UsersTable'
 
 function App() {
   return (
     <div className="App">
       <h1>CRUD Operation</h1>  
+      {/* <AddUser/> */}
       <UsersTable /> 
+      
     </div>
   );
 }
 
 export default App;
+
+
